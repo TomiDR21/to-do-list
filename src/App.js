ff47919f3f8585d67163500bdedb781328d681f9
tomi <tomidirisio@gmail.com>
2025-03-08 13:08:02 -0300

    test change to test branch

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
       <div className={`welcomeMessage ${loaded ? "active" : ""}`}>
-        Welcome to the most complex To-Do List app you've ever seen <br />
-        -Pretty counter-intuitive and difficult to use <br />
-        -Upto 10 To-Do's, avoid procrastination
+        Welcome to the most complex To-Do List app you've ever seen. <br />
+        -Pretty counter-intuitive and difficult to use. <br />
+        -Upto 10 To-Do's, avoid procrastination.
       </div>
       <div className="containerApp">
         <TodoList></TodoList>
